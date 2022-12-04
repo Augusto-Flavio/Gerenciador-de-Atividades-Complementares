26e2dd4d502f5f790bdd53a69854b1a99c303162
Flavio Augusto <flaviosantos.ufra@gmail.com>
2022-12-04 02:29:26 -0300

    feature: Login Activity

--- app/src/main/java/com/example/gerenciadordeatividadescomplementares/Activities/MainActivity.java
+++ app/src/main/java/com/example/gerenciadordeatividadescomplementares/Activities/MainActivity.java
@@ -1,65 +1,52 @@
 package com.example.gerenciadordeatividadescomplementares.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.viewpager.widget.ViewPager;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.example.gerenciadordeatividadescomplementares.Fragments.EnsinoFragment;
-import com.example.gerenciadordeatividadescomplementares.Fragments.ExtensaoFragment;
-import com.example.gerenciadordeatividadescomplementares.Fragments.GestaoFragment;
-import com.example.gerenciadordeatividadescomplementares.Fragments.PesquisaFragment;
 import com.example.gerenciadordeatividadescomplementares.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.ogaclejapan.smarttablayout.SmartTabLayout;
-import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
-import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager viewPager;
-    private SmartTabLayout viewPagerTab;
-    private FloatingActionButton fab;
+    private Button botaoEntrar;
 
-    private String CHANNEL_ID = "CHANNEL ID";
+    private void logar(View view){
+        boolean campoVazio = false;
 
-    //Método que cria um canal de notificações necessário para criar uma notificação
-    private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Canal_GAC", NotificationManager.IMPORTANCE_DEFAULT);
-            channel.setDescription("Canal de notificações do Gerenciador de Atividades Complementares");
+        EditText emailTextInput = findViewById(R.id.emailTextInput);
+        EditText senhaTextInput = findViewById(R.id.senhaTextInput);
+        TextView erroTextView = findViewById(R.id.erroTextView);
 
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
+        String email = emailTextInput.getText().toString();
+        String senha = senhaTextInput.getText().toString();
+        erroTextView.setText("");
+
+        if(email.equals("")){
+            emailTextInput.setError("Você precisa inserir um endereço de e-mail");
+            campoVazio = true;
         }
-    }
 
-    //Método que cria a notificação
-    private void createNotification() {
-        Intent intent = new Intent(this, DocumentsActivity.class);
-        intent.putExtra("Dados", "Documento.pdf");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, intent, 0);
+        if(senha.equals("")){
+            senhaTextInput.setError("Você precisa inserir uma senha");
+            campoVazio = true;
+        }
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_baseline_save_alt_24)
-                .setContentTitle("Documento gerado com sucesso.")
-                .setContentText("Toque aqui para abrir")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
+        if(campoVazio){
+            return;
+        }
 
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-        notificationManager.notify(1, builder.build());
+        if(email.equals("flaviosantos.ufra@gmail.com") && senha.equals("1234")) {
+            Intent intent = new Intent(getApplicationContext(), MyActivitiesActivity.class);
+            startActivity(intent);
+        } else{
+            erroTextView.setText("Endereço de e-mail ou senha incorretos.");
+        }
     }
 
     @Override
@@ -67,35 +54,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createNotificationChannel();
+        // Remover actionbar
+        getSupportActionBar().hide();
 
-        // Remover sombra da ActionBar
-        getSupportActionBar().setElevation(0);
+        botaoEntrar = findViewById(R.id.botaoEntrar);
 
-        // Mudar título da ActionBar
-        getSupportActionBar().setTitle("Minhas Atividades");
-
-        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
-                getSupportFragmentManager(), FragmentPagerItems.with(this)
-                .add("Pesquisa", PesquisaFragment.class)
-                .add("Extensão", ExtensaoFragment.class)
-                .add("Ensino", EnsinoFragment.class)
-                .add("Gestão", GestaoFragment.class)
-                .create());
-
-        viewPager = findViewById(R.id.viewPager);
-        viewPager.setAdapter(adapter);
-
-        viewPagerTab = findViewById(R.id.viewPagerTab);
-        viewPagerTab.setViewPager(viewPager);
-
-        fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
+        botaoEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createNotification();
+                logar(view);
             }
         });
+
     }
 }
