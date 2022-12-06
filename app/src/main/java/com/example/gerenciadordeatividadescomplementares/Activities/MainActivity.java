2a8aa5b9e34d1c12f54ad239a78194f259dcd475
Luiz André <48500507+luizandress@users.noreply.github.com>
2022-12-06 15:37:03 -0300

    Implementa login de usuário pelo Firebase

--- app/src/main/java/com/example/gerenciadordeatividadescomplementares/Activities/MainActivity.java
+++ app/src/main/java/com/example/gerenciadordeatividadescomplementares/Activities/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.gerenciadordeatividadescomplementares.Activities;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.gerenciadordeatividadescomplementares.AtividadeComplementar.AtividadeComplementar;
+import com.example.gerenciadordeatividadescomplementares.AtividadeComplementar.AtividadeComplementar.Tipo;
+import com.example.gerenciadordeatividadescomplementares.Database.CloudDatabaseConnection;
+import com.example.gerenciadordeatividadescomplementares.Database.DatabaseConection;
 import com.example.gerenciadordeatividadescomplementares.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button botaoEntrar;
     private Button botaoCadastrese;
+    private FirebaseAuth firebaseAuth;
 
     private void logar(){
         boolean campoVazio = false;
@@ -42,13 +57,22 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        if(email.equals("flaviosantos.ufra@gmail.com") && senha.equals("1234")) {
-            Intent intent = new Intent(getApplicationContext(), MyActivitiesActivity.class);
-            startActivity(intent);
-        } else{
-            erroTextView.setText("Endereço de e-mail ou senha incorretos.");
-            erroTextView.setVisibility(View.VISIBLE);
-        }
+        firebaseAuth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Intent intent = new Intent(getApplicationContext(), MyActivitiesActivity.class);
+                            startActivity(intent);
+                            Log.d(TAG, "signInWithEmail:success");
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Senha incorreta para esse usuário. Tente \"minhasenha123\".",
+                                    Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, "signInUserWithEmail:failure", task.getException());
+                        }
+                    }
+                });
     }
 
     @Override
@@ -77,5 +101,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Teste de conexão com a API de banco de dados
+        DatabaseConection cloudDatabaseConnection = new CloudDatabaseConnection();
+
+        AtividadeComplementar atividadeComplementar = new AtividadeComplementar("Diprominha", 10.0f, "201901", Tipo.ENSINO, "diprominha.pdf");
+        cloudDatabaseConnection.cadastrarAtividade(atividadeComplementar);
+
+        Log.d(this.getClass().toString(), Integer.toString(cloudDatabaseConnection.getAllAtividades().size()));
+
+        atividadeComplementar.setNome("Dipromão");
+        atividadeComplementar.setArquivo("dipromao.pdf");
+        cloudDatabaseConnection.editarAtividade(atividadeComplementar.getId(), atividadeComplementar);
+        cloudDatabaseConnection.excluirAtividade(atividadeComplementar.getId());
+        Log.d(this.getClass().toString(), Integer.toString(cloudDatabaseConnection.getAllAtividades().size()));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MyActivitiesActivity.class);
+            startActivity(intent);
+        }
     }
 }
